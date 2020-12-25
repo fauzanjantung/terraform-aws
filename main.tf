bd04d9ebb1e323e5ae9ea3f8af9278f342e3afdd
fauzanjantung <fauzan08fauzan@gmail.com>
2020-12-25 10:51:02 +0700

    Terraformars

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-superjantung"
+    versioning {
+        enabled = true
+    }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc"
+}
